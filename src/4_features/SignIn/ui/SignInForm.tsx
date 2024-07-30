42fe01fd204ae0ac3c24838361a10f69309f10ff
Artem Tsyganov <asterionblaze@gmail.com>
2024-07-30 20:14:03 +0300

    Добавил react-hook-form для аутентификации

--- src/4_features/SignIn/ui/SignInForm.tsx
+++ src/4_features/SignIn/ui/SignInForm.tsx
@@ -1,50 +1,55 @@
 "use client";
 import { Button } from "@/6_shared/components";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FormValues } from "../model/signInForm.types";
+import type { SubmitHandler } from "react-hook-form";
 
 export const SignInForm = () => {
-    const [focusState, setFocusState] = useState({
-        email: false,
-        password: false
-    });
+    const { register, handleSubmit, formState } = useForm<FormValues>();
 
-    function checkIsFocused(e: React.FocusEvent, field: "email" | "password") {
-        const target = e.target as HTMLInputElement;
-        if (!target.value) setFocusState({ ...focusState, [field]: false });
-    }
+    const emailError = formState.errors.email?.message;
+    const passwordError = formState.errors.password?.message;
+
+    const submitHandler: SubmitHandler<FormValues> = (data) => {
+        console.log(data);
+    };
 
     return (
-        <form className="col-aligned w-full gap-4">
+        <form
+            className="col-aligned w-full gap-4"
+            onSubmit={handleSubmit(submitHandler)}
+        >
             <div className="relative w-full">
                 <input
-                    onFocus={() =>
-                        setFocusState({ ...focusState, email: true })
-                    }
-                    onBlur={(e) => checkIsFocused(e, "email")}
-                    required
                     type="text"
+                    {...register("email", {
+                        required: "Это поле обязательно!",
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: "Неверная электронная почта!"
+                        }
+                    })}
                     className="w-full rounded-sm border-[1px] border-cyan-dark bg-transparent px-4 py-2 outline-none transition-[border-color] focus:border-cyan-neon"
                 />
                 <span
-                    className={`absolute text-gray-400 ${focusState.email ? "top-0 text-[14px]" : "top-1/2"} pointer-events-none left-2 -translate-y-1/2 bg-blue px-2 transition-all`}
+                    className={`pointer-events-none absolute left-2 top-0 -translate-y-1/2 whitespace-nowrap bg-blue px-2 ${emailError ? "text-red-400" : "text-gray-400"} transition-all`}
                 >
-                    электронная почта
+                    {`электронная почта ${emailError ? "*" : ""}`}
                 </span>
             </div>
             <div className="relative w-full">
                 <input
-                    onFocus={(e) =>
-                        setFocusState({ ...focusState, password: true })
-                    }
-                    onBlur={(e) => checkIsFocused(e, "password")}
-                    required
                     type="text"
+                    {...register("password", {
+                        required: "Это поле обязательно!"
+                    })}
                     className="w-full rounded-sm border-[1px] border-cyan-dark bg-transparent px-4 py-2 outline-none transition-[border-color] focus:border-cyan-neon"
                 />
+
                 <span
-                    className={`absolute text-gray-400 ${focusState.password ? "top-0 text-[14px]" : "top-1/2"} pointer-events-none left-2 -translate-y-1/2 bg-blue px-2 transition-all`}
+                    className={`pointer-events-none ${passwordError ? "text-red-400" : "text-gray-400"} absolute left-2 top-0 -translate-y-1/2 bg-blue px-2 transition-all`}
                 >
-                    пароль
+                    {`пароль ${passwordError ? "*" : ""}`}
                 </span>
             </div>
             <Button
