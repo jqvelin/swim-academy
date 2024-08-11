7e25f5332fe3e041b4746d8d35c49358d6c84d76
Artem Tsyganov <asterionblaze@gmail.com>
2024-08-11 15:07:52 +0300

    Изменение конфига

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "avatars.githubusercontent.com",
-            "lh3.googleusercontent.com",
-            "avatars.yandex.net"
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                port: "",
+                pathname: "/u/**"
+            },
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                port: "",
+                pathname: "/u/**"
+            },
+            {
+                protocol: "https",
+                hostname: "avatars.yandex.net",
+                port: "",
+                pathname: "/**"
+            }
         ]
     }
 };
