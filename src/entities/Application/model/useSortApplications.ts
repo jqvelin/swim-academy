70cc2f8136ed26d94d5f3300c99c2ca9d9163d27
Artem Tsyganov <asterionblaze@gmail.com>
2024-08-14 15:49:21 +0300

    Добавил сортировку для списка заявок

--- src/entities/Application/model/useSortApplications.ts
+++ src/entities/Application/model/useSortApplications.ts
@@ -2,22 +2,42 @@ import { useMemo } from "react";
 
 import { Application } from "./application.types";
 
-export const useSortApplications = (data: Application[]) => {
-    return useMemo(() => {
-        return data?.sort((a, b) => {
-            if (a.isResolved === b.isResolved) {
-                return a.preferred_date > b.preferred_date
-                    ? 1
-                    : a.preferred_date < b.preferred_date
-                      ? -1
-                      : a.preferred_time > b.preferred_time
+export const useSortApplications = (data: Application[], sortBy?: "date" | "name") => {
+    switch(sortBy) {
+        case "name": {
+            return data?.sort((a, b) => {
+                if (a.isResolved === b.isResolved) {
+                    return a.name > b.name
                         ? 1
-                        : -1;
-            } else if (a.isResolved) {
-                return 1;
-            } else {
-                return -1;
-            }
-        });
-    }, [data]);
+                        : a.name < b.name
+                            ? -1
+                            : a.surname > b.surname
+                            ? 1
+                            : -1;
+                } else if (a.isResolved) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            })
+        };
+        default: {
+            return data?.sort((a, b) => {
+                if (a.isResolved === b.isResolved) {
+                    return a.preferred_date > b.preferred_date
+                        ? 1
+                        : a.preferred_date < b.preferred_date
+                            ? -1
+                            : a.preferred_time > b.preferred_time
+                            ? 1
+                            : -1;
+                } else if (a.isResolved) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            });
+        }
+    }
+    
 };
--- src/entities/Application/ui/ApplicationList.tsx
+++ src/entities/Application/ui/ApplicationList.tsx
@@ -8,70 +8,103 @@ import {
     faUser
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useMemo, useState } from "react";
 
 import { useGetAllApplications } from "../model/query-hooks/useGetAllApplications";
 import { useSortApplications } from "../model/useSortApplications";
 import { ApplicationToProcess } from "./ApplicationToProcess";
 
 export const ApplicationList = () => {
+    const [sortBy, setSortBy] = useState<"date" | "name">("date");
     const { data } = useGetAllApplications();
-    const applications = useSortApplications(data || []);
-
+    const applications = useMemo(
+        () => useSortApplications(data || [], sortBy),
+        [data, sortBy]
+    );
     return (
-        <table className="w-11/12 text-center">
-            <thead>
-                <tr>
-                    <th>
-                        <FontAwesomeIcon
-                            icon={faCheck}
-                            className="mr-2"
+        <>
+            <div className="w-11/12 border-cyan-dark border-2 py-4 px-2 rounded-md mb-4">
+                <div>
+                    <span>Сортировка: </span>
+                    <select
+                        onChange={(e) =>
+                            setSortBy(e.target.value as "date" | "name")
+                        }
+                        value={sortBy}
+                        className="text-white bg-transparent border-2 border-cyan-dark rounded-sm p-2"
+                    >
+                        <option
+                            value="date"
+                            className="bg-blue p-2"
+                        >
+                            По дате
+                        </option>
+                        <option
+                            value="name"
+                            className="bg-blue p-2"
+                        >
+                            По имени
+                        </option>
+                    </select>
+                </div>
+            </div>
+            <table className="w-11/12 text-center">
+                <thead>
+                    <tr>
+                        <th>
+                            <FontAwesomeIcon
+                                icon={faCheck}
+                                className="mr-2"
+                            />
+                            <span className="hidden md:inline">Обработана</span>
+                        </th>
+                        <th>
+                            <FontAwesomeIcon
+                                icon={faUser}
+                                className="mr-2"
+                            />
+                            <span className="hidden md:inline">
+                                И.О. клиента
+                            </span>
+                        </th>
+                        <th>
+                            <FontAwesomeIcon
+                                icon={faCalendar}
+                                className="mr-2"
+                            />
+                            <span className="hidden md:inline">
+                                Предпочитаемый день занятия
+                            </span>
+                        </th>
+                        <th>
+                            <FontAwesomeIcon
+                                icon={faClock}
+                                className="mr-2"
+                            />
+                            <span className="hidden md:inline">
+                                Предпочитаемое время занятия
+                            </span>
+                        </th>
+                        <th>
+                            <FontAwesomeIcon
+                                icon={faPhone}
+                                className="mr-2"
+                            />
+                            <span className="hidden md:inline">
+                                Контактный номер телефона
+                            </span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {applications?.map((application) => (
+                        <ApplicationToProcess
+                            key={application.id}
+                            application={application}
                         />
-                        <span className="hidden md:inline">Обработана</span>
-                    </th>
-                    <th>
-                        <FontAwesomeIcon
-                            icon={faUser}
-                            className="mr-2"
-                        />
-                        <span className="hidden md:inline">И.О. клиента</span>
-                    </th>
-                    <th>
-                        <FontAwesomeIcon
-                            icon={faCalendar}
-                            className="mr-2"
-                        />
-                        <span className="hidden md:inline">
-                            Предпочитаемый день занятия
-                        </span>
-                    </th>
-                    <th>
-                        <FontAwesomeIcon
-                            icon={faClock}
-                            className="mr-2"
-                        />
-                        <span className="hidden md:inline">
-                            Предпочитаемое время занятия
-                        </span>
-                    </th>
-                    <th>
-                        <FontAwesomeIcon
-                            icon={faPhone}
-                            className="mr-2"
-                        />
-                        <span className="hidden md:inline">
-                            Контактный номер телефона
-                        </span>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {applications?.map((application) => (
-                    <ApplicationToProcess
-                        key={application.id}
-                        application={application}
-                    />
-                ))}
-            </tbody>
-        </table>
+                    ))}
+                </tbody>
+            </table>
+        </>
     );
 };
